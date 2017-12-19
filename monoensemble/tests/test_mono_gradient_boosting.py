e519a5ab9d6c0b7d3b0af32adfc9b783a323948e
chriswbartley <chris@empirikalsystems.com>
2017-12-19 13:22:13 +0800

    AV

--- monoensemble/tests/test_mono_gradient_boosting.py
+++ monoensemble/tests/test_mono_gradient_boosting.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, division, print_function
 
 import numpy as np
 import numpy.testing as npt
-from mono_gradient_boosting import MonoGradientBoostingClassifier
+from monoensemble.mono_gradient_boosting import MonoGradientBoostingClassifier
 from sklearn.datasets import load_boston
 
 
