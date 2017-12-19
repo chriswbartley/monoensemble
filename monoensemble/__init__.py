d6a5eb99ddc1763f3fd1c2152c89ba4eae9215e4
chriswbartley <chris@empirikalsystems.com>
2017-12-19 14:54:17 +0800

    AV

---
+++ monoensemble/__init__.py
@@ -0,0 +1,8 @@
+# from __future__ import absolute_import, division, print_function
+# from .version import __version__  # noqa
+# from ._mono_gradient_boosting import *  # noqa
+from mono_gradient_boosting import MonoGradientBoostingClassifier  # noqa
+from mono_forest import MonoRandomForestClassifier  # noqa
+#from .monoensemble import MonoGradientBoostingClassifier  # noqa
+
+__all__ = ["MonoGradientBoostingClassifier", "MonoRandomForestClassifier"] # noqa
