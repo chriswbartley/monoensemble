1d15459115b0a08abcd151ab5905d1d6b290b320
chriswbartley <chris@empirikalsystems.com>
2017-12-20 11:32:13 +0800

    PP

--- monoensemble/__init__.py
+++ monoensemble/__init__.py
@@ -1,6 +1,8 @@
 # from __future__ import absolute_import, division, print_function
 # from .version import __version__  # noqa
-from ._mono_gradient_boosting import *  # noqa
+from ._mono_gradient_boosting import apply_rules_c, get_node_map_c
+from ._mono_gradient_boosting import update_rule_coefs, update_rule_coefs_newton_step
+from ._mono_gradient_boosting import _random_sample_mask
 from .mono_gradient_boosting import MonoGradientBoostingClassifier  # noqa
 from .mono_forest import MonoRandomForestClassifier  # noqa
 
