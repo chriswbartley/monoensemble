930489a53a02f53d9ce280bd8266078a300ba2db
chriswbartley <chris@empirikalsystems.com>
2017-12-20 08:38:55 +0800

    PP

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ def generate_a_pyrex_source(self, base, ext_name, source, extension):
 
 build_src.build_src.generate_a_pyrex_source = generate_a_pyrex_source
 ########################
-# END additionnal code #
+# END additional code  #
 ########################
 
 
@@ -72,7 +72,7 @@ def configuration(parent_package="", top_path=None):
     config = Configuration("monoensemble", parent_package, top_path)
     config.add_extension("_mono_gradient_boosting",
                          sources=["monoensemble/_mono_gradient_boosting.pyx"],
-                         include_dirs=[numpy.get_include()])
+                         include_dirs=['.', numpy.get_include()])
 
     #config.add_subpackage("tests")
 
