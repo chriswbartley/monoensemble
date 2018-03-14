83bd04b6b5431077e96b0563d64996aa0533a1b3
chriswbartley <chris@empirikalsystems.com>
2018-03-15 06:13:01 +0800

    1

--- monoensemble/__init__.py
+++ monoensemble/__init__.py
@@ -5,6 +5,7 @@ from ._mono_gradient_boosting import update_rule_coefs, update_rule_coefs_newton
 from ._mono_gradient_boosting import _random_sample_mask
 from ._mono_gradient_boosting import get_node_map_and_rule_feats_c
 from ._mono_gradient_boosting import apply_rules_rule_feat_cache_c
+#from .tree import DecisionTreeClassifier, DecisionTreeRegressor
 from .mono_gradient_boosting import MonoGradientBoostingClassifier  # noqa
 from .mono_forest import MonoRandomForestClassifier  # noqa
 
