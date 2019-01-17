5bd465e3a0065d9f01050a423e6cd41661dd96d3
chriswbartley <chris@empirikalsystems.com>
2019-01-17 11:59:54 +0800

    ref

--- monoensemble/setup.py
+++ monoensemble/setup.py
@@ -79,11 +79,11 @@ def configuration(parent_package="", top_path=None):
 #                         include_dirs=[numpy.get_include()],
 #                         libraries=libraries,
 #                         extra_compile_args=["-O3"])
-    config.add_extension("_splitter_mt",
-                         sources=["_splitter_mt.pyx"],
-                         include_dirs=[numpy.get_include()],#
-                         libraries=libraries,
-                         extra_compile_args=["-O3"])
+#    config.add_extension("_splitter_mt",
+#                         sources=["_splitter_mt.pyx"],
+#                         include_dirs=[numpy.get_include()],#
+#                         libraries=libraries,
+#                         extra_compile_args=["-O3"])
 #    config.add_extension("_criterion",
 #                         sources=["_criterion.pyx"],
 #                         include_dirs=[numpy.get_include()],
